742f2be85147b4074b66a06dd9e88dd246d7a8e8
Matthew Jones <matts3371@gmail.com>
2020-04-06 21:35:23 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,4 @@ class Post < ActiveRecord::Base
   validates :content, length: {minimum: 250} 
   validates :summary, length: {maximum: 250}
   validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "%{value} is not a valid category, please enter 'Fiction' or 'Non-Fiction'."}
-  validates :title, inclusion: {in: %w(Believe Secret Top Guess)}
 end
